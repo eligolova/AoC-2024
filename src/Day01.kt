6a4aa2863ae3eda4b4bd7279f344b65cb0bc2dcb
eligogo <lisa.golova@gmail.com>
2024-12-01 22:30:46 +0000

    Day1 Part2: Initial solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,11 +13,28 @@ fun main() {
         return left.zip(right) { l, r -> abs(l - r) }.sum()
     }
 
+    fun part2(input: List<String>): Int {
+        //fetch the sorted lists
+        val (left, right) = input
+            .map { it.trim().split("\\s+".toRegex()) }
+            .map { it[0].toInt() to it[1].toInt() }
+            .unzip()
+        val counts = right.groupingBy { it }.eachCount()
+
+        var similarityScore = 0
+        left.forEach{
+            similarityScore += it * counts.getOrDefault(it, 0)}
+
+        return similarityScore
+    }
+
     // Read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+    part2(input).println()
 }
