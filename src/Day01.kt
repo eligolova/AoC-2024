b03c9fc63cc81252ae0e412fbc0376f6f23b0ff0
eligogo <lisa.golova@gmail.com>
2024-12-02 22:42:43 +0000

    Day2 Part2: Some functional changes. Small refactor for test input.

--- src/Day01.kt
+++ src/Day01.kt
@@ -22,13 +22,13 @@ fun main() {
         return left.fold(0) { acc, num -> acc + num * counts.getOrDefault(num, 0) }
     }
 
-    // Read a large test input from the `src/Day01_test.txt` file:
+    // Read a large test input from the `src/test_input/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
     check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+//    // Read the input from the `src/test_input/Day01.txt` file.
+//    val input = readInput("Day01")
+//    part1(input).println()
+//    part2(input).println()
 }
